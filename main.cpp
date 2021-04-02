aae53101865c17f9c042cf39bd2e6dc870b0ea92
seeker_ <71497793+proovethetruth@users.noreply.github.com>
2021-04-02 15:10:24 +0300

    Corrected commentary

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 
-// Написать генератор псевдослучайных чисел по алгоритму
+// Create pseudo-random number generator, based on algorythm:
 // f(n) = f(n - 1) + f(n - 2) + f(n - 3) + f(n - 4) mod 211;
 // f(0) = f(1) = f(2) = f(3) = 1
-// Подсчитать частоту каждого числа при выборке 100 000.
+// Count frequency of each number when sampling 100.000
 
 #include <stdio.h>
 #include <stdlib.h>
